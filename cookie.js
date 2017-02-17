8ad427c58d64023d2a1820fd025587258c6a6f14
pie5434 <sallypoon22@gmail.com>
2017-02-17 00:23:12 +0000

    Message

---
+++ cookie.js
@@ -0,0 +1,12 @@
+data.happy{
+   "happy":[
+    "Fortune",
+    "Fortune",
+    ],
+    "dark":[
+    "fortune",
+    "fortune",
+    ]
+}
+
+var fortune = { Happy: "Fortune", Sad: "Fortune", Angry, Mean, Cocky, Dark, Dank Memes ,
